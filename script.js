6fefa77149423eb9b0dc424735b94bf5f7fd229a
gamadany35-hub <gamadany35@gmail.com>
2026-08-31 09:11:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ const games = [
     cat: "Football",
     icon: "⚽",
     url: "https://www.mediafire.com/file/piakyxymvyv1723/PES2025_NBC_PL_V2_BY_MASTER_PES.zip/file"
-  }{
-  name: "PES 2021 Mobile UCL MOD OFFLINE",
-  platform: "Android",
-  size: "1.62 GB",
-  cat: "Football",
-  icon: "⚽",
-  url: "https://modsfire.com/download/8n21dAo36O351Q3/68715"
-}
+  },
+
+  {
+    name: "PES 2021 Mobile UCL MOD OFFLINE",
+    platform: "Android",
+    size: "1.62 GB",
+    cat: "Football",
+    icon: "⚽",
+    url: "https://modsfire.com/download/8n21dAo36O351Q3/68715"
+  }
 ];
-}
+
 const gameList = document.getElementById("gameList");
 const search = document.getElementById("search");
 const count = document.getElementById("count");
@@ -26,15 +28,10 @@ function showGames(list) {
   count.textContent =
     list.length + (list.length === 1 ? " game" : " games");
 
-  if (list.length === 0) {
-    gameList.innerHTML =
-      "<p>Hakuna game iliyopatikana 🔎</p>";
-    return;
-  }
-
-  list.forEach(game => {
+  list.forEach(function(game) {
 
     const card = document.createElement("div");
+
     card.className = "game-card";
 
     card.innerHTML = `
@@ -45,15 +42,16 @@ function showGames(list) {
       <h3>${game.name}</h3>
 
       <p>🎮 Platform: ${game.platform}</p>
+
       <p>📦 Size: ${game.size}</p>
+
       <p>🗂️ Category: ${game.cat}</p>
 
       <a
         class="download-btn"
         href="${game.url}"
         target="_blank"
-        rel="noopener noreferrer"
-      >
+        rel="noopener noreferrer">
         📥 DOWNLOAD
       </a>
     `;
@@ -66,25 +64,29 @@ function filterCategory(category) {
 
   if (category === "All") {
     showGames(games);
-    return;
+  } else {
+
+    const filtered = games.filter(function(game) {
+      return game.cat === category;
+    });
+
+    showGames(filtered);
   }
-
-  const filtered = games.filter(
-    game => game.cat === category
-  );
-
-  showGames(filtered);
 }
 
-search.addEventListener("input", function () {
+search.addEventListener("input", function() {
 
   const text = search.value.toLowerCase();
 
-  const results = games.filter(game =>
-    game.name.toLowerCase().includes(text) ||
-    game.platform.toLowerCase().includes(text) ||
-    game.cat.toLowerCase().includes(text)
-  );
+  const results = games.filter(function(game) {
+
+    return (
+      game.name.toLowerCase().includes(text) ||
+      game.platform.toLowerCase().includes(text) ||
+      game.cat.toLowerCase().includes(text)
+    );
+
+  });
 
   showGames(results);
 });
